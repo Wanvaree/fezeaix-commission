2f000f03a90298e081f35a6eee7dd46aad880dfb
DESKTOP-AHLU5P6\User <nosei2559@gmail.com>
2025-11-08 20:45:46 +0700

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/fezeaix-commission/sw.js', { scope: '/fezeaix-commission/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
